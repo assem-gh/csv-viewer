a3ba81939c17441fbace388a36af65e97b7508d1
asem259 <alghabra.asem@gmail.com>
2023-01-27 17:45:48 +0100

    initial project.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { MantineProvider } from "@mantine/core";
+
+function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      App
+    </MantineProvider>
+  );
+}
+
+export default App;
